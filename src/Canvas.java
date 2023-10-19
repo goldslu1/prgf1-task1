4105907f845cf77c7a768d7b345417680ccdf7b3
Lukáš Goldšmíd <goldslu1@uhk.cz>
2023-10-19 13:56:27 +0200

    Removed drawing circles. Removed some unnecessary code. Added clearing the panel with the press of C key.

--- src/Canvas.java
+++ src/Canvas.java
@@ -1,16 +1,12 @@
 import objectdata.Point;
 import rasterdata.Presentable;
-import rasterdata.Raster;
 import rasterdata.RasterBI;
-import rasterop.CirclerTrivial;
 import rasterop.LinerTrivial;
 
 import java.awt.BorderLayout;
-import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Graphics;
 import java.awt.event.*;
-import java.awt.image.BufferedImage;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
@@ -31,8 +27,6 @@ public class Canvas {
 	private JFrame frame;
 	private JPanel panel;
 	private LinerTrivial ln;
-	private CirclerTrivial cr;
-	//private BufferedImage img;
 	private RasterBI img; //TODO prepsat s vlastni tridou, kdyz uzivatel klikne oznaci se bod a tazenim mysi ? + dalsi bod = usecka
 	private Presentable prs;
 	private Point p1;
@@ -49,7 +43,6 @@ public class Canvas {
 		frame.setResizable(false);
 		frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
 
-		//img = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);  //aby jsme nastavili barvu pixelu
 		img = new RasterBI(width, height);
 		background = 0x2f2f2f;
 		panel = new JPanel() {
@@ -72,42 +65,8 @@ public class Canvas {
 		panel.addKeyListener(new KeyAdapter() {
 			@Override
 			public void keyPressed(KeyEvent e) {
-				if (e.getKeyCode() == KeyEvent.VK_UP) {  //pri stisknuti tlacitka se bude posunovat jako had
-					y--;
+				if (e.getKeyCode() == KeyEvent.VK_C) {
 					img.clear(background);
-					img.setColor(x, y, 0xffff00);
-					img.setColor(x, y, 0xffff00);
-					img.setColor(x + 1, y, 0xffff00);
-					img.setColor(x - 1, y, 0xffff00);
-					img.setColor(x, y - 1, 0xffff00);
-					img.setColor(x, y + 1, 0xffff00);
-					img.present(panel.getGraphics());
-				} else if (e.getKeyCode() == KeyEvent.VK_DOWN) {
-					y++;
-					img.clear(background);
-					img.setColor(x, y, 0xffff00);
-					img.setColor(x + 1, y, 0xffff00);
-					img.setColor(x - 1, y, 0xffff00);
-					img.setColor(x, y - 1, 0xffff00);
-					img.setColor(x, y + 1, 0xffff00);
-					img.present(panel.getGraphics());
-				} else if (e.getKeyCode() == KeyEvent.VK_LEFT) {
-					x--;
-					img.clear(background);
-					img.setColor(x, y, 0xffff00);
-					img.setColor(x + 1, y, 0xffff00);
-					img.setColor(x - 1, y, 0xffff00);
-					img.setColor(x, y - 1, 0xffff00);
-					img.setColor(x, y + 1, 0xffff00);
-					img.present(panel.getGraphics());
-				} else if (e.getKeyCode() == KeyEvent.VK_RIGHT) {
-					x++;
-					img.clear(background);
-					img.setColor(x, y, 0xffff00);
-					img.setColor(x + 1, y, 0xffff00);
-					img.setColor(x - 1, y, 0xffff00);
-					img.setColor(x, y - 1, 0xffff00);
-					img.setColor(x, y + 1, 0xffff00);
 					img.present(panel.getGraphics());
 				}
 			}
@@ -127,7 +86,7 @@ public class Canvas {
 		panel.addMouseListener(new MouseAdapter() {
 			@Override
 			public void mousePressed(MouseEvent e) {
-				img.clear(background);
+				//img.clear(background);
 				p1 = new Point(e.getX(), e.getY());
 				mouseDragged = false;
 				img.present(panel.getGraphics());
@@ -147,36 +106,18 @@ public class Canvas {
 		panel.addMouseListener(new MouseAdapter() {
 			@Override
 			public void mouseReleased(MouseEvent e) {
-				img.clear(background);
+				//img.clear(background);
 				p2 = new Point(e.getX(), e.getY());
 				mouseDragged = true;
 				ln = new LinerTrivial();
-				cr = new CirclerTrivial();
 				ln.drawLine(img, p1, p2,0xffff00);
-				cr.drawCircle(img, p1, 0xffff00);
 				img.present(panel.getGraphics());
 			}
 		});
 	}
 
-	/*public void clear() {
-		Graphics gr = img.getGraphics();
-		gr.setColor(new Color(0x2f2f2f));
-		gr.fillRect(0, 0, img.getWidth(), img.getHeight());
-	}*/
-
-	/*public void present(Graphics graphics) {
-		graphics.drawImage(img, 0, 0, null);
-	}*/
-
 	public void draw() {
 		img.clear(background);
-		/*img.setColor(img.getWidth() / 2 , img.getHeight() / 2, 0xffff00);
-		img.setColor((img.getWidth() / 2) + 1 , img.getHeight() / 2, 0xffff00);
-		img.setColor((img.getWidth() / 2) - 1 , img.getHeight() / 2, 0xffff00);
-		img.setColor(img.getWidth() / 2 , (img.getHeight() / 2) + 1, 0xffff00);
-		img.setColor(img.getWidth() / 2 , (img.getHeight() / 2) - 1, 0xffff00);*/
-
 	}
 
 	public void start() {
